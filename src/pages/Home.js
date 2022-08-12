5b4a17de38a66d6988293a74012a605d359ca4cc
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-08-12 23:16:08 +0430

    Home component added!

---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email'
+import GithubIcon from '@material-ui/icons/GitHub'
+import '../styles/Home.css'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='about'>
+                <h2>Hi, my name is Mohsen</h2>
+                <div className='prompt'>
+                    <p>A software developer with a passion for learning and creating!</p>
+                    <LinkedInIcon />
+                    <EmailIcon />
+                    <GithubIcon />
+                </div>
+            </div>
+            <div className='skills'>
+                <h1>Skills</h1>
+                <ol className='list'>
+                    <li className='item'>
+                        <h2>Front-End</h2>
+                        <span>
+                            ReactJS, Redux, HTML, CSS, NPM, Yarn
+                        </span>
+                    </li>
+                    <li className='item'>
+                        <h2>Languages</h2>
+                        <span>
+                            JavaScript, Python
+                        </span>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
