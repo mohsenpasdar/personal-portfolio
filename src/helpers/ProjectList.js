8b39227b1fea4869e5b0b52c7b8367bd0b4b440c
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-08-15 23:30:32 +0430

    almost done except for some styling related to projectDisplay_3

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,31 +9,31 @@ export const ProjectList = [
     {
         name: "Dijkstra Algorithm Visualizer",
         image: Proj1,
-        skills: "JavaScript,HTML,CSS",
+        skills: "JavaScript, HTML, CSS",
     },
     {
         name: "Ecommerce Website",
         image: Proj2,
-        skills: "React,Node.js,MongoDB",
+        skills: "React, Node.js, MongoDB",
     },
     {
         name: "Spotify Clone",
         image: Proj3,
-        skills: "React,Node.js,MongoDB,SpotifyAPI",
+        skills: "React, Node.js, MongoDB, SpotifyAPI",
     },
     {
         name: "Social Media Website",
         image: Proj4,
-        skills: "React,Node.js,MySQL,GraphQL",
+        skills: "React, Node.js, MySQL, GraphQL",
     },
     {
         name: "Dashboard Visualizer",
         image: Proj5,
-        skills: "JavaScript,HTML,CSS",
+        skills: "JavaScript, HTML, CSS",
     },
     {
         name: "Mobile Game",
         image: Proj6,
-        skills: "React Native,JavaScript,HTML,CSS",
+        skills: "React Native, JavaScript, HTML, CSS",
     },
 ];
